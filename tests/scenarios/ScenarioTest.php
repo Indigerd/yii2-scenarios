788204b75920ebc573cd21c4a319b8b37bff4e17
indigerd <alex.stepanenko@gmail.com>
2019-04-05 17:04:08 +0300

    added tests

--- tests/scenarios/ScenarioTest.php
+++ tests/scenarios/ScenarioTest.php
@@ -8,8 +8,14 @@
 
 namespace indigerd\scenariostests;
 
+use indigerd\scenarios\exception\RequestValidateException;
 use indigerd\scenarios\Scenario;
+use indigerd\scenarios\validation\factory\ValidatorCollectionFactory;
+use indigerd\scenarios\validation\factory\ValidatorFactory;
+use indigerd\scenarios\validation\validator\IntegerValidator;
+use indigerd\scenarios\validation\validator\RequiredValidator;
 use PHPUnit\Framework\TestCase;
+use yii\web\Request;
 
 class ScenarioTest extends TestCase
 {
@@ -17,6 +23,34 @@ class ScenarioTest extends TestCase
 
     protected function setUp() : void
     {
-        //$this->scenario = new Scenario();
+        $this->scenario = new Scenario(new ValidatorFactory(), new ValidatorCollectionFactory());
+    }
+
+    public function testValidateRequest()
+    {
+        $params = [
+            'user_id' => 1,
+            'firstName' => 'John',
+        ];
+        $request = new Request();
+        $request->setBodyParams($params);
+        $this->scenario->addValidator('user_id', new IntegerValidator());
+        $this->scenario->addValidator('user_id', new RequiredValidator());
+        $this->scenario->addValidator('firstName', new RequiredValidator());
+        $this->assertTrue($this->scenario->validateRequest($request));
+    }
+
+    public function testValidateRequestException()
+    {
+        $this->expectException(RequestValidateException::class);
+        $params = [
+            'user_id' => 1,
+        ];
+        $request = new Request();
+        $request->setBodyParams($params);
+        $this->scenario->addValidator('user_id', new IntegerValidator());
+        $this->scenario->addValidator('user_id', new RequiredValidator());
+        $this->scenario->addValidator('firstName', new RequiredValidator());
+        $this->assertTrue($this->scenario->validateRequest($request));
     }
 }
